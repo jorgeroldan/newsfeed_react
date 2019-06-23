321440063e9440b75f800a372de1f774218ee537
Jorge Roldán <roldan21@gmail.com>
2019-06-23 16:17:07 -0300

    gitignore

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './index.css'
+import Hero from '../Hero'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Hero />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Hero/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Hero extends Component {
+  render() {
+    return (
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Hola Devs4Good</h1>
+          </div>
+        </div>
+      </section>
+    )
+  }
+}
+
+export default Hero
